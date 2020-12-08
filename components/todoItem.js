f7abe7a2bae62431b357ed6eacf75063277dda01
benoit de kersabiec <benoit.dekersabiec@student-cs.fr>
2020-12-08 07:11:58 +0100

    adding checkboxes next to items text

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 export default function TodoItem({ item, pressHandler, is_done }) {
+    const [toggleCheckBox, setToggleCheckBox] = useState(is_done);
+
+    const handleCheckBox = (newValue) => {
+        setToggleCheckBox(newValue);
+        pressHandler(item.key);
+    };
+
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={is_done? styles.striked_through_item : styles.item}>{item.text}</Text>
+            <View style={styles.container}>
+                <CheckBox
+                    value={toggleCheckBox}
+                    onValueChange={handleCheckBox}
+                />
+                <Text style={is_done? styles.striked_through_item : styles.item}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
-    item: {
+    container: {
         padding: 16,
         marginTop: 5,
         borderColor: '#EEEEEE',
         borderWidth: 1,
         borderStyle: 'solid',
         borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    item:{
+        paddingLeft: 15,
     },
     striked_through_item: {
-        padding: 16,
-        marginTop: 5,
+        paddingLeft: 15,
         color: "#999999",
-        borderColor: '#EEEEEE',
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderRadius: 10,
         textDecorationLine: "line-through",
         textDecorationStyle: "solid",
     }
